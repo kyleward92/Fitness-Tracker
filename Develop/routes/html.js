ff64d040238059ed0c2377241aeba4bcdc1cc55c
unknown <kyleward1992@gmail.com>
2021-01-25 13:57:59 -0500

    adjusting routes

---
+++ Develop/routes/html.js
@@ -0,0 +1,18 @@
+// const express = require('express')
+// const path = require('path')
+// const app = express();
+
+//   app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/public/index.html"));
+// });
+
+// app.get("/exercise", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/public/exercise.html"));
+// });
+
+// app.get("/stats", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/public/stats.html"));
+// });
+
+
+// // module.exports = router;
