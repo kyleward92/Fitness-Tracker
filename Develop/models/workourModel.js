e503d6aab9778c5fc71f6ef560679bfca85bd867
unknown <kyleward1992@gmail.com>
2021-01-25 13:48:40 -0500

    index.js

---
+++ Develop/models/workourModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter type of workout"
+              }, 
+              name: {
+                type: String,
+                trim: true,
+                required: "Enter a name for workout"
+              },
+              duration: {
+                type: Number,
+              },
+              weight: {
+                type: Number,
+              },
+              reps: {
+                type: Number,
+              },
+              sets: {
+                type: Number,
+              },
+              distance: {
+                type: Number,
+              },
+        }
+    ]
+})
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
